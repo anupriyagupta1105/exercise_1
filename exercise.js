1b244663597c59689dc29d9cf087562486e7b5ad
anupriyagupta1105 <anupriyagupta1105@gmail.com>
2015-06-19 11:52:26 -0400

    update

---
+++ exercise.js
@@ -0,0 +1,89 @@
+//Question 1
+var name= "Rob";
+//Question2
+var greeting= "Hello" + " " + name;
+//checking q1/2
+console.log(name)
+console.log(greeting)
+//Question3
+//find the value of true and false and not true and store it to a variable called success
+
+var success = (true)
+//Question4
+//Question5
+function myName(){
+	return "BrainStation"
+}
+//Question6
+console.log(myName())
+//Question7
+//create a unction with the paramter name that logs the string "hello space name"
+function Name(name){
+	return "Hello" + " " + name
+}
+console.log(Name("Anupriya"))
+//Question8
+function booLean(a,b){
+	return a||b
+}
+console.log(booLean(true, false))
+//Question9
+//We use Git to share projects/code online in the open space community. It can also be used
+//to work on projects in a group setting where we
+//Question10
+var value= 5
+function If(value){
+	if (value >=2) {
+		return true;
+	}
+	else{
+		return false;
+	}
+};
+
+//Question11 
+	console.log("Hello World")
+	console.log("Hello World")
+	console.log("Hello World")
+	console.log("Hello World")
+	console.log("Hello World")
+
+//Question12
+var myNumbers= [0,1,2,3,4,5]
+console.log(myNumbers)
+//Question13
+function add(myNumbers){
+	return 0+1+2+3+4+5
+}
+console.log(add(myNumbers))
+//Question14
+//NEED MORE TIME TO COMPLETE- UNDERSTAND IT 
+//add numbers 12-55 inclusive
+for (var i=12;i>=55;i++);
+console.log(add(i));
+
+//Question15
+//constructor- should take name and address as parameters
+function person(name, address){
+		this.name= name;
+		this.address=address;
+	}
+//question 16
+//DIDN"T UNDERSTAND MOST OF THE QUESTIONS AFTER THIS.
+function Computer()
+{
+	this.memory= memory;
+	this.screenSize= screenSize;
+}
+
+
+//question17
+
+
+//question18
+function ComputerStore(){
+	this.name=name;
+	this.address= address;
+
+	method.makeComputer= "create a computer object: given some memory and screen size"
+}
